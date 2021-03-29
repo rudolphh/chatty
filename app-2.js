9ea069d3e27ab3829c2d0cace1afe836b9b3906d
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-29 08:54:55 -0700

    async version

--- app-2.js
+++ app-2.js
@@ -14,6 +14,24 @@ const client = redis.createClient({
 
 const apiUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=';
 
+
+app.get('/async/:key', async (req, res) => {
+
+    let key = encodeURI(req.params.key);
+
+    try {
+      const value = await axios.get(`${apiUrl}${key}`); 
+       return res.status(200).send({
+        error: false,
+        data: value.data
+      });
+     
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
 app.get('/:queryKey', (req, res) => {
 
     let queryKey = encodeURI(req.params.queryKey);
@@ -24,14 +42,14 @@ app.get('/:queryKey', (req, res) => {
             // we have the value of the key set in redis
             console.log('from cache');
             let jsonValue = JSON.parse(strValue); // parse the value string into json
-            res.json(jsonValue);
+            res.send(jsonValue.parse.text['*']);
         } else {
             
             // there was no key so make an api call
             console.log('from api');
             axios.get(apiUrl + queryKey).then(response => {
                 client.set(queryKey, JSON.stringify(response.data));// store json obj as string
-                res.json(response.data);
+                res.send(response.data.parse.text['*']);
             });
         }
     });
